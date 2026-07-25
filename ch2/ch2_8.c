d48d35d7950a36a737b93683ef014fdf771ce90a
kimyuhan <yuhankim2006@gmail.com>
2026-07-25 15:58:53 +0900

    ch2 8번 연습문제를 풀음

---
+++ ch2/ch2_8.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int one_three();
+
+
+int main(void)
+{
+    printf("stating now:\n");
+    one_three();
+    printf("done!\n");
+    
+    return 0;
+}
+
+int one_three()
+{
+    printf("one\n");
+    printf("two\n");
+    printf("three\n");
+    
+    return 0;
+}
