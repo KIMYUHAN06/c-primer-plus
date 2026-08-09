0acb12d5aacee704d94e1db5c89e4b8988f7552e
kimyuhan <yuhankim2006@gmail.com>
2026-08-09 14:24:34 +0900

    divide.c 구현

---
+++ ch5/divide.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("정수형 나눗셈:  5/4   is %d \n", 5/4);
+    printf("정수형 나눗셈:  6/3   is %d \n", 6/3);
+    printf("정수형 나눗셈:  7/4   is %d \n", 7/4);
+    printf("부동소수점형 나눗셈: 7./4. is %1.2f \n", 7./4.);
+    printf("혼합된 나눗셈:    7./4  is %1.2f \n", 7./4);
+    
+    return 0;
+}
